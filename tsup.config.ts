f82a48cf1aa21725c0a0722c835013d980d4ec64
Thu Htet Naing <thuhtetnaing@i.viabells.com>
2024-02-20 08:44:54 +0630

    update npm packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,10 @@ import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entryPoints: ["index.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  splitting: false,
+  sourcemap: true,
   clean: true,
-  format: ["cjs"],
-  ...options,
+  // ...options,
 }));
